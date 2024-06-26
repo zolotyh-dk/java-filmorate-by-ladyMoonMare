483186ccec4cbf0c6c239ed8c50d5bd540653dd2
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-06-26 19:04:52 +0300

    Created deserializers for fields LocalDate and duration

--- src/main/java/ru/yandex/practicum/filmorate/util/deserializer/DurationDeserializer.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/deserializer/DurationDeserializer.java
@@ -1,2 +1,16 @@
-package ru.yandex.practicum.filmorate.util.deserializer;public class DurationDeserializer {
+package ru.yandex.practicum.filmorate.util.deserializer;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class DurationDeserializer extends JsonDeserializer<Duration> {
+    @Override
+    public Duration deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        return Duration.parse("PT" + jsonParser.getValueAsString() + "M");
+    }
 }
