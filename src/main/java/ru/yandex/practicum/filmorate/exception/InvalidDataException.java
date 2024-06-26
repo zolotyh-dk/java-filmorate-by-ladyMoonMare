affb9544201352fb50317a380fd9fdcef2df71d4
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-06-26 19:02:21 +0300

    Created exception for invalid data

--- src/main/java/ru/yandex/practicum/filmorate/exception/InvalidDataException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InvalidDataException.java
@@ -1,2 +1,10 @@
-package ru.yandex.practicum.filmorate.exception;public class InvalidDataException {
+package ru.yandex.practicum.filmorate.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
 }
