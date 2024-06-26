aa46d6cf7cfffe1bde7f4532cefe4f86464a5b97
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-06-26 19:03:11 +0300

    Created user and film models

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,2 +1,30 @@
-package ru.yandex.practicum.filmorate;public class User {
+package ru.yandex.practicum.filmorate.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.*;
+import ru.yandex.practicum.filmorate.util.deserializer.LocalDateDeserializer;
+import ru.yandex.practicum.filmorate.util.serializer.LocalDateSerializer;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private int id;
+    private String email;
+    private String login;
+    private String name;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate birthday;
+
+    public User(String email, String login, String name, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+
 }
