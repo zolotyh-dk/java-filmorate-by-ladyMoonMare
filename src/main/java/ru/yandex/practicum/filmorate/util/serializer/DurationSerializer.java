66594fe7991aa59e87bd7882a366d59db65411df
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-06-26 19:04:35 +0300

    Created serializers for fields LocalDate and duration

--- src/main/java/ru/yandex/practicum/filmorate/util/serializer/DurationSerializer.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/serializer/DurationSerializer.java
@@ -1,28 +1,15 @@
 package ru.yandex.practicum.filmorate.util.serializer;
 
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import java.time.Duration;
 
-public class DurationSerializer extends TypeAdapter<Duration> {
-    @Override
-    public void write(JsonWriter jsonWriter, Duration duration) throws IOException {
-        if (duration != null) {
-            jsonWriter.value(duration.toMinutes());
-        } else {
-            jsonWriter.nullValue();
-        }
-    }
+public class DurationSerializer extends JsonSerializer<Duration> {
 
     @Override
-    public Duration read(JsonReader jsonReader) throws IOException {
-        String str = "PT" + jsonReader.nextString() + "M";
-        if (str.equals("PTM")) {
-            return null;
-        }
-        return Duration.parse(str);
+    public void serialize(Duration duration, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeNumber(duration.toMinutes());
     }
 }
