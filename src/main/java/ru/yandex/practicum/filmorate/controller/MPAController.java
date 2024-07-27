ec9d39ebc9b0bc7e4368237f5b23877983a1b0ba
Valeriya Chechuro <lera.chechuro@yandex.ru>
2024-07-28 01:36:48 +0300

    test fix

--- src/main/java/ru/yandex/practicum/filmorate/controller/MPAController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MPAController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.MPA;
 import ru.yandex.practicum.filmorate.service.BaseService;
 
